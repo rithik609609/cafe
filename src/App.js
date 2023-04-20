7dc4d307595e25123550f2027c6e013cc7d814d5
ankit <ankit@themetromaxgroup.com>
2023-04-21 00:50:24 +0530

    half done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './component/Card';
+import Banner from './pages/banner';
+import Header from "./pages/header"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header/>
+      <Banner/>
+      <div style={{display:"grid", gridTemplateColumns:"1fr 1fr"}}>
+      <Card/>
+      <Card/>
+      <Card/>
+      </div>
     </div>
   );
 }
---
+++ src/pages/banner/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./banner.css"
+const Banner = () => {
+  return (
+    <div className='main_banner_div'>
+    <div className='banner'> <span> Welcome to <br/>Sacred Earth </span></div>
+    </div>
+  )
+}
+
+export default Banner
